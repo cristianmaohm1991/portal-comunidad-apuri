2e666af3a5ac9cee760acf35fb45dd1f9310a910
cristianmaohm1991 <cristianmao1991@gmail.com>
2026-09-09 01:17:16 -0500

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://lrwewsaigjpvhgkpcpdp.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "sb_publishable_sdMjDpcJeQ0M7aRcqjOm4A_2FSS5DJ5";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
